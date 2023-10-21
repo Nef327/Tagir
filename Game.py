323868831f9c8d02d93df0bcf2ee736b4ce52eef
Nef327 <grigoriirusinkuprash@gmail.com>
2023-10-21 16:43:25 +0400

    Project start - camera pos/scale functions

--- Game.py
+++ Game.py
@@ -1,17 +1,32 @@
 import pyglet as pg
+from pyglet.math import Mat4
 
 
 class Game:
     def __init__(self):
         self.window = pg.window.Window()
         self.camera_pos = pg.math.Vec3(self.window.view[0], self.window.view[5], self.window.view[10])
-        self.camera_scale = pg.math.Vec3(self.window.view[0], self.window.view[5], self.window.view[10])
+        self.scale: int = 1
 
         self.objects = []
         self.global_params = {}
 
-    def change_camera_pos(self, x: float, y: float, z: float):
-        self.window.view.translate(pg.math.Vec3(x, y, z))
+    def change_camera_pos(self, x: float = 0, y: float = 0, z: float = 0):
+        self.camera_pos += pg.math.Vec3(x, y, z)
+        self.window.view += self.window.view.translate(pg.math.Vec3(x, y, z))
 
+    def set_camera_pos(self, x: float = 0, y: float = 0, z: float = 0):
+        self.camera_pos = pg.math.Vec3(x, y, z)
+        self.window.view = self.window.view.translate(pg.math.Vec3(x, y, z))
 
-    def change_
+    def add_scale(self, n):
+        self.scale = n
+        self.window.view = self.window.view.scale(n)
+
+    def set_scale(self, n):
+        self.window.view = self.window.view.scale(1 / self.scale)
+        self.scale = n
+
+    def scale_to_default(self):
+        self.window.view = self.window.view.scale(1 / self.scale)
+        self.scale = 1
